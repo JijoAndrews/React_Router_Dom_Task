8b149ad9add72cb95ba6ee98b257284c7925bc6b
JijoAndrews <jijoandrews07@gmail.com>
2024-11-06 16:40:05 +0530

    cleanp in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import { Tempnavbar } from './Tempnavbar'
 import './App.css'
